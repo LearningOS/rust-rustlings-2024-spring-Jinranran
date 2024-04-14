ec21f17ff3365771f0dc3d776495c3889f256f49
Jinranran <2955081687@qq.com>
2024-04-14 16:49:52 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,17 +7,25 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let trim=input.trim();
+    trim.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut what=String::from(input);
+    what.push_str(" world!");
+    what
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    if input.contains("cars"){
+    let replaced=input.replace("cars","balloons");
+    replaced}
+    else{
+        input.to_string()
+    }
 }
 
 #[cfg(test)]
